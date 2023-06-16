a8abead8b82f89f417e54790edd177b27a66a344
l7g9 <lukewgregory@gmail.com>
2023-06-16 13:22:09 +0100

    feat(load_balancer): add arn to outputs

--- modules/load_balancer/outputs.tf
+++ modules/load_balancer/outputs.tf
@@ -2,3 +2,8 @@ output "lb_dns_name" {
   value       = aws_lb.main.dns_name
   description = "DNS name assigned to load balancer"
 }
+
+output "lb_arn" {
+  value       = aws_lb.main.id
+  description = "ARN assigned to load balancer"
+}
